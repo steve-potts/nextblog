2969c9b54ac243f75039dbfdcd9aef249f249e11
Steve Potts <coolpot_1@yahoo.com>
2024-07-26 01:39:44 +0100

    upgrade nextjs to 14 and add auth0 basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function Page() {
   return (
     <main>
-
+      <h1>Home Page</h1>
+      <Link href="/api/auth/login">Login</Link>
     </main>
   )
 }
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <p>User Page</p>
+    </main>
+  )
+}
