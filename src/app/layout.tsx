3de9afe90f7fdb3bb90222f6b972269016b01fb3
Steve Potts <coolpot_1@yahoo.com>
2024-07-31 14:17:13 +0100

    Basic state done with recoil for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
+"use client";
 import Navbar from '@/components/nav/Navbar';
 import './globals.css'
-export const metadata = {
-  title: 'NextJS',
-  description: 'NextJS',
-}
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import Sidebar from '@/components/nav/Sidebar';
+import { RecoilRoot } from 'recoil';
 
 export default function RootLayout({
   children,
@@ -15,13 +13,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body className='bg-gray-50 w-full h-screen overflow-clip flex flex-col'>
-          <Navbar/>
-          <main className='w-full h-full flex flex-col md:flex-row'>
-            <Sidebar/>
-            <div className='w-full md:pr-32 overflow-auto'>{children}</div>
-          </main>
-        </body>
+        <RecoilRoot>
+          <body className='bg-gray-50 w-full h-screen overflow-clip flex flex-col'>
+            <Navbar/>
+            <main className='w-full h-full flex flex-col md:flex-row'>
+              <Sidebar/>
+              <div className='w-full md:pr-32 overflow-auto'>{children}</div>
+            </main>
+          </body>
+        </RecoilRoot>
       </UserProvider>
     </html>
   )
